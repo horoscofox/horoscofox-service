56d49d73fa21958ceb4d7f6467fd7d26a2554454
sᴀʟᴠᴀᴛᴏʀᴇ <svt.busiello@gmail.com>
2018-04-13 11:41:24 +0200

    Remove hardcoded parameters for development server 🙊

--- horoscofox-service/src/app.py
+++ horoscofox-service/src/app.py
@@ -1,9 +1,10 @@
 from urls import routes
 from apistar import ASyncApp, Route
-
+from settings import DEV_SERVER_ADDR, DEV_SERVER_PORT, DEV_USE_DEBUGGER, DEV_USE_RELOADER
 app = ASyncApp(
     routes=routes
 )
 
 if __name__ == '__main__':
-    app.serve('0.0.0.0', 9001, use_debugger=True, use_reloader=True)
+    app.serve(DEV_SERVER_ADDR, DEV_SERVER_PORT,
+              use_debugger=DEV_USE_DEBUGGER, use_reloader=DEV_USE_RELOADER)
--- horoscofox-service/src/connection.py
+++ horoscofox-service/src/connection.py
@@ -1,3 +1,4 @@
 from mongoengine import connect
+from settings import MONGO_CONNECTION_HOST, MONGO_CONNECTION_NAME
 
-connect('horoscofox', host='mongodb://database/horoscofox')
+connect(MONGO_CONNECTION_NAME, host=MONGO_CONNECTION_HOST)
